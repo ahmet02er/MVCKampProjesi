873b56957258bdaa577ac53ecb6d9a829bb97e90
AO <ahmet_ozer@hotmail.com>
2021-09-28 17:18:54 +0300

    28.09.2021

---
+++ BusinessLayer/ValidationRules/FluentValidation/MessageValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+   public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.MessageReceiverMail).NotEmpty().WithMessage("Alıcı E-Posta Adresi Boş Olamaz...");
+            RuleFor(x => x.MessageSubject).NotEmpty().WithMessage("Konu Boş Olamaz...");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesaj Boş Olamaz...");
+            RuleFor(x => x.MessageSubject).MinimumLength(3).WithMessage("Konu En Az ( 3 ) Karakter Olması Gerekiyor...");
+            RuleFor(x => x.MessageSubject).MaximumLength(50).WithMessage("Konu En Fazla ( 50 ) Karakterden Fazla Olamaz...");
+        }
+    }
+}
---
+++ MVCKampProjesiUI/Controllers/AdminMessageController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKampProjesiUI.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        MessageManager messageManager  = new MessageManager(new EfMessageDal());
+        //MessageValidator validationRules = new MessageValidator();
+        public ActionResult Inbox()
+        {
+            var messageValue = messageManager.GetListInbox();
+            return View(messageValue);
+        }
+        public ActionResult Sendbox()
+        {
+            var messageValue = messageManager.GetListSendbox();
+            return View(messageValue);
+        }
+
+        public ActionResult InboxMessageDetails(int id)
+        {
+            var messageValue = messageManager.GetById(id);
+            return View(messageValue);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+            return View();
+        }
+    }
+}
